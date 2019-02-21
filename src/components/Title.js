e18a14b5f42e4d20d365d6f1ac5b766a5cbae7fc
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-02-21 13:03:31 +0100

    Add Title component

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Title.css';
+
+const Title = props => {
+  return (
+    <div className={style.Title}>
+      <h1>{props.title}</h1>
+      <p>Number of todo tasks: {props.number}</p>
+    </div>
+  );
+};
+
+export default Title;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
+import Title from '../components/Title';
 
 class App extends React.Component {
   constructor(props) {
@@ -24,6 +25,7 @@ class App extends React.Component {
   render() {
     return (
       <div className={style.TodoApp}>
+        <Title title="ToDo Application Webpack + React" number={this.state.data.length} />
         Tutaj pojawią się komponenty naszej aplikacji.
       </div>
     );
